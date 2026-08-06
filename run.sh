31d756eaf5274d3697d214db5f410849926ded66
Max NuttShell <NuttShell@users.noreply.github.com>
2026-08-06 12:22:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ if [ -f "token_extractor.zip" ]; then
 fi
 
 curl --silent --fail --show-error --location --remote-name --remote-header-name\
-  https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
+  https://github.com/NuttShell/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
 unzip token_extractor.zip
 cd token_extractor
 pip3 install -r requirements.txt
