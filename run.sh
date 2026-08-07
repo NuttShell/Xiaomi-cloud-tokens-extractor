509d7ac11c5f0c14cfb2f884b28679783ce8876a
Max NuttShell <NuttShell@users.noreply.github.com>
2026-08-07 12:22:34 +0700

    small fix

--- run.sh
+++ run.sh
@@ -3,21 +3,20 @@ ha_host=$(ha network info --raw-json 2>/dev/null | jq -r '[.data.interfaces[] | 
 if [ -z "${ha_host}" ]; then
   ha_host="127.0.0.1"
 fi
-host_to_pass=$ha_host || "127.0.0.1"
+host_to_pass="$ha_host"
 set -o errexit  # fail on first error
 set -o nounset  # fail on undef var
 set -o pipefail # fail on first error in pipe
-
 if [ -f "token_extractor.zip" ]; then
     echo "token_extractor.zip file already exists, please remove it and try again..."
     exit 1
 fi
-
 curl --silent --fail --show-error --location --remote-name --remote-header-name\
   https://github.com/NuttShell/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
 unzip token_extractor.zip
 cd token_extractor
 pip3 install -r requirements.txt
-python3 token_extractor.py --host $host_to_pass
+python3 token_extractor.py --serve-image --host $host_to_pass
 cd ..
+cp token_extractor/xiaomi_tokens_*.txt . 2>/dev/null || true
 rm -rf token_extractor token_extractor.zip
